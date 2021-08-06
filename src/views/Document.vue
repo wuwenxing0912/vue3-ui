f34a38de6f9b8924131f0a327777470177de6ed3
wuwenxing <1428560819@qq.com>
2021-08-06 08:49:50 +0800

    更改样式

--- src/views/Document.vue
+++ src/views/Document.vue
@@ -37,8 +37,11 @@ export default {
 $main-color: #42b983;
 .document {
 	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
 	> .content {
 		display: flex;
+		flex-grow: 1;
 		aside {
 			// width: 150px;
 			// padding: 16px;
@@ -91,123 +94,6 @@ $main-color: #42b983;
 			flex-grow: 1;
 			padding: 16px;
 			font-size: 16px;
-
-			> section {
-				max-width: 1000px;
-				padding: 20px;
-				margin: 0px auto;
-				> h1 {
-					margin-bottom: 20px;
-				}
-				& .example-container {
-					margin: 0 auto;
-					border: 1px solid rgba(0, 0, 0, 0.15);
-					border-top-left-radius: 4px;
-					border-top-right-radius: 4px;
-					padding: 40px;
-					display: flex;
-					justify-content: flex-start;
-					align-items: center;
-					& .x-scroll-inner {
-						& p {
-							padding: 5px 10px;
-							margin: 10px;
-							color: rgba(0, 0, 0, 0.65);
-							border: 1px solid rgba(0, 0, 0, 0.05);
-						}
-					}
-				}
-				& .example-container-fix {
-					margin: 0 auto;
-					border: 1px solid rgba(0, 0, 0, 0.15);
-					border-top-left-radius: 4px;
-					border-top-right-radius: 4px;
-					padding: 40px;
-				}
-				& .description {
-					display: flex;
-					justify-content: space-between;
-					align-items: center;
-					top: -1px;
-					position: relative;
-					border: 1px solid rgba(0, 0, 0, 0.15);
-					border-top: none;
-					border-bottom-left-radius: 4px;
-					border-bottom-right-radius: 4px;
-					padding: 20px 40px;
-					font-size: 14px;
-					color: rgba(0, 0, 0, 0.65);
-
-					& .pop-content {
-						display: inline-block;
-						position: absolute;
-						right: 0;
-						top: -30%;
-						padding: 6px;
-						transform: translateX(-25%);
-						background: #000000bf;
-						color: #fff;
-						border-radius: 2px;
-					}
-
-					& .pop-content::after {
-						content: "";
-						display: inline-block;
-						border: 6px solid #000000bf;
-						position: absolute;
-						right: 50%;
-						transform: translateX(50%);
-						top: 100%;
-						border-bottom-color: transparent;
-						border-left-color: transparent;
-						border-right-color: transparent;
-					}
-				}
-
-				& .code-area {
-					pre {
-						padding: 20px 0;
-					}
-					pre div {
-						padding: 6px 40px;
-						font-family: "Courier New", ui-monospace, -webkit-pictograph;
-					}
-				}
-
-				& .api-table {
-					border: 1px solid #f0f0f0;
-					font-size: 14px;
-					color: rgba(0, 0, 0, 0.65);
-					width: 100%;
-					margin: 0 auto;
-					border-collapse: collapse;
-					text-align: left;
-					vertical-align: middle;
-					& th:first-child,
-					& td:first-child {
-						border-left: 1px solid #f0f0f0;
-					}
-					& tr {
-						border-top: 1px solid #f0f0f0;
-						border-bottom: 1px solid #f0f0f0;
-					}
-					& th,
-					& td {
-						font-weight: normal;
-						padding: 16px;
-					}
-					> thead {
-						color: rgba(0, 0, 0, 0.65);
-						background-color: rgba(0, 0, 0, 0.04);
-					}
-					& tbody tr:hover {
-						background-color: rgba(0, 0, 0, 0.02);
-					}
-					& .api-type {
-						color: #c41d7f;
-					}
-				}
-			}
 		}
 	}
 }

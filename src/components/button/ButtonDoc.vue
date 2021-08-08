3f0f9885670fae427c16eb341ba261e43ba406ed
wuwenxing <1428560819@qq.com>
2021-08-08 13:40:17 +0800

    重构 展示页

--- src/components/button/ButtonDoc.vue
+++ src/components/button/ButtonDoc.vue
@@ -1,27 +1,5 @@
 <template>
-	<section>
-		<h1>组件和代码示例</h1>
-		<div class="example-container">
-			<Button :levle="levle" :size="size">按钮</Button>
-		</div>
-		<div class="description">
-			<span class="text">开关的基本用法</span>
-			<span
-				class="code-icon"
-				@click="changeCodeVisible"
-				@mouseenter="changeCodeTipVisible"
-				@mouseleave="changeCodeTipVisible"
-			>
-				<svg class="icon">
-					<use xlink:href="#icon-code"></use>
-				</svg>
-			</span>
-			<span class="pop-content" v-if="codeTipVisible">
-				{{ codeVisible ? "收起代码" : "显示代码" }}
-			</span>
-		</div>
-		<div class="code-area" v-if="codeVisible">code</div>
-	</section>
+	<ButtonDefault />
 	<section>
 		<h1>API</h1>
 		<table class="api-table" cellSpacing="0" cellPadding="0">
@@ -39,33 +17,12 @@
 </template>
 
 <script lang="ts">
-import { ref } from "vue";
-import Button from "./Button.vue";
+import ButtonDefault from "./example/ButtonDefault.vue";
 export default {
-	components: { Button: Button },
-	setup() {
-		const codeVisible = ref(false);
-		const changeCodeVisible = () => {
-			codeVisible.value = !codeVisible.value;
-		};
-		const codeTipVisible = ref(false);
-		const changeCodeTipVisible = () => {
-			codeTipVisible.value = !codeTipVisible.value;
-		};
-		const levle = "primary";
-		const size = "middle";
-		return {
-			codeVisible,
-			changeCodeVisible,
-			codeTipVisible,
-			changeCodeTipVisible,
-			levle,
-			size,
-		};
-	},
+	components: { ButtonDefault },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import "../../style/example.scss";
 </style>

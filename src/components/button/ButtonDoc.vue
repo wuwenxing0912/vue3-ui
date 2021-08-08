005c2df55ca8b8bbd334e0d3497f1a1945187215
wuwenxing <1428560819@qq.com>
2021-08-08 09:58:38 +0800

    新增 Button 组件

---
+++ src/components/button/ButtonDoc.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<Button>按钮</Button>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Button from "./Button.vue";
+export default {
+	components: { Button: Button },
+};
+</script>
--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,7 @@ import { createWebHashHistory, createRouter } from "vue-router";
 import Home from "./views/Home.vue";
 import Document from "./views/Document.vue";
 import SwitchDoc from "./components/switch/SwitchDoc.vue";
-import Button from "./components/Button.vue";
+import ButtonDoc from "./components/button/ButtonDoc.vue";
 import Dialog from "./components/Dialog.vue";
 import Tabs from "./components/Tabs.vue";
 
@@ -16,7 +16,7 @@ export const router = createRouter({
 			component: Document,
 			children: [
 				{ path: "switch", component: SwitchDoc },
-				{ path: "button", component: Button },
+				{ path: "button", component: ButtonDoc },
 				{ path: "dialog", component: Dialog },
 				{ path: "tabs", component: Tabs },
 			],

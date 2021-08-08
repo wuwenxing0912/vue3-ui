5250d8243b2e316bda8d3b30b2a3a803725b2bad
wuwenxing <1428560819@qq.com>
2021-08-08 14:36:27 +0800

    添加 展示示例

--- src/components/button/ButtonDoc.vue
+++ src/components/button/ButtonDoc.vue
@@ -1,25 +1,31 @@
 <template>
-	<ButtonDefault />
-	<section>
-		<h1>API</h1>
-		<table class="api-table" cellSpacing="0" cellPadding="0">
-			<thead>
-				<tr>
-					<th class="api-table-head">API</th>
-					<th class="api-table-head">说明</th>
-					<th class="api-table-head">类型</th>
-					<th class="api-table-head">默认值</th>
-				</tr>
-			</thead>
-			<tbody></tbody>
-		</table>
-	</section>
+  <ButtonDefault />
+  <ButtonDisabled />
+  <ButtonSize />
+  <ButtonLoading />
+  <section>
+    <h1>API</h1>
+    <table class="api-table" cellSpacing="0" cellPadding="0">
+      <thead>
+        <tr>
+          <th class="api-table-head">API</th>
+          <th class="api-table-head">说明</th>
+          <th class="api-table-head">类型</th>
+          <th class="api-table-head">默认值</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  </section>
 </template>
 
 <script lang="ts">
 import ButtonDefault from "./example/ButtonDefault.vue";
+import ButtonDisabled from "./example/ButtonDisabled.vue";
+import ButtonSize from "./example/ButtonSize.vue";
+import ButtonLoading from "./example/ButtonLoading.vue";
 export default {
-	components: { ButtonDefault },
+  components: { ButtonDefault, ButtonDisabled, ButtonLoading, ButtonSize },
 };
 </script>
 
---
+++ src/components/button/example/ButtonDisabled.vue
@@ -0,0 +1,51 @@
+<template>
+  <section>
+    <div class="example-container">
+      <Button levle="default" style="margin-right: 20px" disabled>Default </Button>
+      <Button levle="primary" style="margin-right: 20px" disabled>Primary</Button>
+      <Button levle="dashed" style="margin-right: 20px" disabled>Dashed</Button>
+      <Button levle="danger" disabled>Danger</Button>
+    </div>
+    <div class="description">
+      <span class="text">按钮禁用：设置按钮 disabled 禁用状态。</span>
+      <span
+        class="code-icon"
+        @click="changeCodeVisible"
+        @mouseenter="changeCodeTipVisible"
+        @mouseleave="changeCodeTipVisible"
+      >
+        <svg class="icon">
+          <use xlink:href="#icon-code"></use>
+        </svg>
+      </span>
+      <span class="pop-content" v-if="codeTipVisible">
+        {{ codeVisible ? "收起代码" : "显示代码" }}
+      </span>
+    </div>
+    <div class="code-area" v-if="codeVisible">code</div>
+  </section>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+import Button from "../Button.vue";
+export default {
+  components: { Button: Button },
+  setup() {
+    const codeVisible = ref(false);
+    const changeCodeVisible = () => {
+      codeVisible.value = !codeVisible.value;
+    };
+    const codeTipVisible = ref(false);
+    const changeCodeTipVisible = () => {
+      codeTipVisible.value = !codeTipVisible.value;
+    };
+    return {
+      codeVisible,
+      changeCodeVisible,
+      codeTipVisible,
+      changeCodeTipVisible,
+    };
+  },
+};
+</script>

5dcb9a0427e3ef052cedbaa6e98e32d48827e2ba
wuwenxing <1428560819@qq.com>
2021-08-09 13:48:55 +0800

    添加 展示示例

--- src/components/modal/ModalDoc.vue
+++ src/components/modal/ModalDoc.vue
@@ -1,12 +1,47 @@
 <template>
-  <div>
-    <Modal />
-  </div>
+  <ModalDefault />
+  <section>
+    <h1>API</h1>
+    <table class="api-table" cellSpacing="0" cellPadding="0">
+      <thead>
+        <tr>
+          <th class="api-table-head">API</th>
+          <th class="api-table-head">说明</th>
+          <th class="api-table-head">类型</th>
+          <th class="api-table-head">默认值</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  </section>
 </template>
 
 <script lang="ts">
-import Modal from "./Modal.vue";
+import ModalDefault from "./example/ModalDefault.vue";
+import { ref } from "vue";
 export default {
-  components: { Modal },
+  components: {
+    ModalDefault,
+  },
+  setup() {
+    const codeVisible = ref(false);
+    const changeCodeVisible = () => {
+      codeVisible.value = !codeVisible.value;
+    };
+    const codeTipVisible = ref(false);
+    const changeCodeTipVisible = () => {
+      codeTipVisible.value = !codeTipVisible.value;
+    };
+    return {
+      codeVisible,
+      changeCodeVisible,
+      codeTipVisible,
+      changeCodeTipVisible,
+    };
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+@import "../../style/example.scss";
+</style>
---
+++ src/components/modal/example/ModalDefault.vue
@@ -0,0 +1,50 @@
+<template>
+  <section>
+    <div class="example-container">
+      <Button levle="primary"> open Modal </Button>
+      <Modal />
+    </div>
+    <div class="description">
+      <span class="text">对话框的基本用法。</span>
+      <span
+        class="code-icon"
+        @click="changeCodeVisible"
+        @mouseenter="changeCodeTipVisible"
+        @mouseleave="changeCodeTipVisible"
+      >
+        <svg class="icon">
+          <use xlink:href="#icon-code"></use>
+        </svg>
+      </span>
+      <span class="pop-content" v-if="codeTipVisible">
+        {{ codeVisible ? "收起代码" : "显示代码" }}
+      </span>
+    </div>
+    <div class="code-area" v-if="codeVisible">code</div>
+  </section>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+import Button from "../../button/Button.vue";
+import Modal from "../Modal.vue";
+export default {
+  components: { Button, Modal },
+  setup() {
+    const codeVisible = ref(false);
+    const changeCodeVisible = () => {
+      codeVisible.value = !codeVisible.value;
+    };
+    const codeTipVisible = ref(false);
+    const changeCodeTipVisible = () => {
+      codeTipVisible.value = !codeTipVisible.value;
+    };
+    return {
+      codeVisible,
+      changeCodeVisible,
+      codeTipVisible,
+      changeCodeTipVisible,
+    };
+  },
+};
+</script>

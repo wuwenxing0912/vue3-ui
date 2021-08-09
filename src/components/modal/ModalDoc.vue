7df6ce04a422e0380b4e2b43578689612ba370f4
wuwenxing <1428560819@qq.com>
2021-08-09 15:50:36 +0800

    新增 展示页 API表格

--- src/components/modal/ModalDoc.vue
+++ src/components/modal/ModalDoc.vue
@@ -11,7 +11,56 @@
           <th class="api-table-head">默认值</th>
         </tr>
       </thead>
-      <tbody></tbody>
+      <tbody>
+        <tr>
+          <td class="api">visible</td>
+          <td class="api-description">Modal 是否可见</td>
+          <td class="api-type">boolean</td>
+          <td class="api-default">false</td>
+        </tr>
+        <tr>
+          <td class="api">onClickMaskClose</td>
+          <td class="api-description">是否点击遮罩层关闭 Modal</td>
+          <td class="api-type">boolean</td>
+          <td class="api-default">false</td>
+        </tr>
+        <tr>
+          <td class="api">cancelButtonLevle</td>
+          <td class="api-description">Modal 取消按钮 类型</td>
+          <td class="api-type">"default" | "primary" | "dashed" | "danger"</td>
+          <td class="api-default">default</td>
+        </tr>
+        <tr>
+          <td class="api">confirmButtonLevle</td>
+          <td class="api-description">Modal 确定按钮 类型</td>
+          <td class="api-type">"default" | "primary" | "dashed" | "danger"</td>
+          <td class="api-default">primary</td>
+        </tr>
+        <tr>
+          <td class="api">onClose</td>
+          <td class="api-description">关闭 Modal 回调函数</td>
+          <td class="api-type">Function</td>
+          <td class="api-default">---</td>
+        </tr>
+        <tr>
+          <td class="api">onConfirm</td>
+          <td class="api-description">确定 Modal 回调函数</td>
+          <td class="api-type">Function</td>
+          <td class="api-default">---</td>
+        </tr>
+        <tr>
+          <td class="api">cancelButtonSize</td>
+          <td class="api-description">Modal 取消按钮 尺寸大小</td>
+          <td class="api-type">"large" | "middle" | "small"</td>
+          <td class="api-default">middle</td>
+        </tr>
+        <tr>
+          <td class="api">confirmButtonLevle</td>
+          <td class="api-description">Modal 确定按钮 尺寸大小</td>
+          <td class="api-type">"large" | "middle" | "small"</td>
+          <td class="api-default">middle</td>
+        </tr>
+      </tbody>
     </table>
   </section>
 </template>

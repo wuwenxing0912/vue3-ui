160c7af6ea5c23a0fb6773c32a90ef9a0099e507
wuwenxing <1428560819@qq.com>
2021-08-09 13:29:50 +0800

    修改 Modal 路由

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,7 @@ import Home from "./views/Home.vue";
 import Document from "./views/Document.vue";
 import SwitchDoc from "./components/switch/SwitchDoc.vue";
 import ButtonDoc from "./components/button/ButtonDoc.vue";
-import Dialog from "./components/Dialog.vue";
+import ModalDoc from "./components/modal/ModalDoc.vue";
 import Tabs from "./components/Tabs.vue";
 
 const history = createWebHashHistory();
@@ -17,7 +17,7 @@ export const router = createRouter({
 			children: [
 				{ path: "switch", component: SwitchDoc },
 				{ path: "button", component: ButtonDoc },
-				{ path: "dialog", component: Dialog },
+				{ path: "modal", component: ModalDoc },
 				{ path: "tabs", component: Tabs },
 			],
 		},
--- src/views/Document.vue
+++ src/views/Document.vue
@@ -1,100 +1,100 @@
 <template>
-	<div class="document">
-		<TopNav />
-		<div class="content">
-			<aside>
-				<h2>组件</h2>
-				<ol>
-					<li>
-						<router-link to="/document/switch">Switch 开关</router-link>
-					</li>
-					<li>
-						<router-link to="/document/button">Button 按钮</router-link>
-					</li>
-					<li>
-						<router-link to="/document/dialog">Dialog 对话框</router-link>
-					</li>
-					<li>
-						<router-link to="/document/tabs">Tabs 标签页</router-link>
-					</li>
-				</ol>
-			</aside>
-			<main><router-view></router-view></main>
-		</div>
-	</div>
+  <div class="document">
+    <TopNav />
+    <div class="content">
+      <aside>
+        <h2>组件</h2>
+        <ol>
+          <li>
+            <router-link to="/document/switch">Switch 开关</router-link>
+          </li>
+          <li>
+            <router-link to="/document/button">Button 按钮</router-link>
+          </li>
+          <li>
+            <router-link to="/document/modal">Modal 对话框</router-link>
+          </li>
+          <li>
+            <router-link to="/document/tabs">Tabs 标签页</router-link>
+          </li>
+        </ol>
+      </aside>
+      <main><router-view></router-view></main>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import TopNav from "../components/TopNav.vue";
 export default {
-	components: {
-		TopNav,
-	},
+  components: {
+    TopNav,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 $main-color: #42b983;
 .document {
-	min-height: 100vh;
-	display: flex;
-	flex-direction: column;
-	> .content {
-		display: flex;
-		flex-grow: 1;
-		aside {
-			// width: 150px;
-			// padding: 16px;
-			// > h2 {
-			// 	margin-bottom: 4px;
-			// }
-			// > ol {
-			// 	> li {
-			// 		padding: 4px 0;
-			// 		margin-left: 8px;
-			// 	}
-			// }
-			// @media (max-width: 500px) {
-			// 	position: fixed;
-			// 	top: 0;
-			// 	left: 0;
-			// 	padding-top: 80px;
-			// }
-			border-right: 1px solid #eeeeee;
-			margin-right: 32px;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  > .content {
+    display: flex;
+    flex-grow: 1;
+    aside {
+      // width: 150px;
+      // padding: 16px;
+      // > h2 {
+      // 	margin-bottom: 4px;
+      // }
+      // > ol {
+      // 	> li {
+      // 		padding: 4px 0;
+      // 		margin-left: 8px;
+      // 	}
+      // }
+      // @media (max-width: 500px) {
+      // 	position: fixed;
+      // 	top: 0;
+      // 	left: 0;
+      // 	padding-top: 80px;
+      // }
+      border-right: 1px solid #eeeeee;
+      margin-right: 32px;
 
-			> h2 {
-				font-size: 20px;
-				padding: 48px 16px 6px 30px;
-			}
-			> ol {
-				min-width: 12em;
-				margin-left: 12px;
-				> li > a {
-					font-size: 15px;
-					height: 40px;
-					padding: 4px 16px 4px 30px;
-					display: flex;
-					justify-content: flex-start;
-					align-items: center;
-					color: rgba(0, 0, 0, 0.75);
+      > h2 {
+        font-size: 20px;
+        padding: 48px 16px 6px 30px;
+      }
+      > ol {
+        min-width: 12em;
+        margin-left: 12px;
+        > li > a {
+          font-size: 15px;
+          height: 40px;
+          padding: 4px 16px 4px 30px;
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+          color: rgba(0, 0, 0, 0.75);
 
-					&:hover {
-						color: $main-color;
-					}
-					&.router-link-active {
-						color: $main-color;
-						border-right: 1px solid $main-color;
-						background: rgba($main-color, 0.15);
-					}
-				}
-			}
-		}
-		main {
-			flex-grow: 1;
-			padding: 16px;
-			font-size: 16px;
-		}
-	}
+          &:hover {
+            color: $main-color;
+          }
+          &.router-link-active {
+            color: $main-color;
+            border-right: 1px solid $main-color;
+            background: rgba($main-color, 0.15);
+          }
+        }
+      }
+    }
+    main {
+      flex-grow: 1;
+      padding: 16px;
+      font-size: 16px;
+    }
+  }
 }
 </style>

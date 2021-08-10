c84f2b55afbc96344a52c17001c4a369dab8219d
wuwenxing <1428560819@qq.com>
2021-08-10 18:13:04 +0800

    实现 下划线 滑动

--- src/components/Tabs/Tabs.vue
+++ src/components/Tabs/Tabs.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="x-tabs">
-    <div class="x-tabs-nav">
+    <div class="x-tabs-nav" ref="nav">
       <div
         class="x-tabs-nav-item"
         v-for="(title, index) in titles"
         :key="index"
         :class="{ selected: title === selected }"
         @click="selectTitle(title)"
+        :ref="
+          (el) => {
+            if (el) navItems[index] = el;
+          }
+        "
       >
         {{ title }}
       </div>
+      <span class="x-tabs-nav-item-underline" ref="underline"></span>
     </div>
     <div class="x-tabs-content">
       <component class="x-tabs-content-item" :is="currentContent" :key="selected" />
@@ -19,7 +25,7 @@
 
 <script lang="ts">
 import Tab from "./Tab.vue";
-import { computed } from "vue";
+import { computed, ref, onMounted, onUpdated } from "vue";
 export default {
   props: {
     selected: {
@@ -44,7 +50,29 @@ export default {
         return child.props.title === props.selected;
       })[0];
     });
-    return { children, titles, selectTitle, currentContent };
+    const nav = ref<HTMLDivElement>(null);
+    const navItems = ref<HTMLDivElement[]>([]);
+    const underline = ref<HTMLSpanElement>(null);
+    const setUnderlinePosition = () => {
+      const selectedItem = navItems.value.filter((item) => {
+        return item.classList.contains("selected");
+      })[0];
+      const navLeft = nav.value.getBoundingClientRect().left;
+      const { width, left } = selectedItem.getBoundingClientRect();
+      underline.value.style.width = `${width}px`;
+      underline.value.style.left = `${left - navLeft}px`;
+    };
+    onMounted(setUnderlinePosition);
+    onUpdated(setUnderlinePosition);
+    return {
+      children,
+      titles,
+      selectTitle,
+      currentContent,
+      nav,
+      navItems,
+      underline,
+    };
   },
 };
 </script>
@@ -53,6 +81,7 @@ export default {
 $main-color: #007aff;
 .x-tabs {
   &-nav {
+    position: relative;
     display: flex;
     color: #333;
     border-bottom: 1px solid #d9d9d9;
@@ -66,6 +95,15 @@ $main-color: #007aff;
       &.selected {
         color: $main-color;
       }
+    }
+    &-item-underline {
+      display: inline-block;
+      position: absolute;
+      height: 3px;
+      background: $main-color;
+      left: 0;
+      bottom: -1px;
+      transition: all 0.25s;
     }
   }
   &-content {

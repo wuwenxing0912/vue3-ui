f5b94e5f17440f41dafd50039a63fc135b099dfc
wuwenxing <1428560819@qq.com>
2021-08-10 16:29:42 +0800

    Tabs 样式

--- src/components/Tabs/Tabs.vue
+++ src/components/Tabs/Tabs.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    <div v-for="(title, index) in titles" :key="index">{{ title }}</div>
-    <component v-for="(child, index) in children" :is="child" :key="index" />
+  <div class="x-tabs">
+    <div class="x-tabs-nav">
+      <div class="x-tabs-nav-item" v-for="(title, index) in titles" :key="index">
+        {{ title }}
+      </div>
+    </div>
+    <div class="x-tabs-content">
+      <component
+        class="x-tabs-content-item"
+        v-for="(child, index) in children"
+        :is="child"
+        :key="index"
+      />
+    </div>
   </div>
 </template>
 
@@ -22,3 +33,28 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+$main-color: #007aff;
+.x-tabs {
+  &-nav {
+    display: flex;
+    color: #333;
+    border-bottom: 1px solid #d9d9d9;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $main-color;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>

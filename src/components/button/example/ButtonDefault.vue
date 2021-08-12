fb8a9cfbfbae3956bb039489a6dd7708d7f9c120
wuwenxing <1428560819@qq.com>
2021-08-12 16:51:34 +0800

    重构 ButtonDefault 组件

--- src/components/button/example/ButtonDefault.vue
+++ src/components/button/example/ButtonDefault.vue
@@ -1,52 +1,38 @@
 <template>
-  <section>
-    <h1>组件和代码示例</h1>
-    <div class="example-container">
-      <Button levle="default" style="margin-right: 20px">Default</Button>
-      <Button levle="primary" style="margin-right: 20px">Primary</Button>
-      <Button levle="dashed" style="margin-right: 20px">Dashed</Button>
-      <Button levle="danger">Danger</Button>
-    </div>
-    <div class="description">
-      <span class="text">按钮有四种类型：默认按钮、主题按钮、虚线按钮以及危险按钮。</span>
-      <span
-        class="code-icon"
-        @click="changeCodeVisible"
-        @mouseenter="changeCodeTipVisible"
-        @mouseleave="changeCodeTipVisible"
-      >
-        <svg class="icon">
-          <use xlink:href="#icon-code"></use>
-        </svg>
-      </span>
-      <span class="pop-content" v-if="codeTipVisible">
-        {{ codeVisible ? "收起代码" : "显示代码" }}
-      </span>
-    </div>
-    <div class="code-area" v-if="codeVisible">code</div>
-  </section>
+  <Example
+    title="组件和代码示例"
+    description="按钮有四种类型：默认按钮、主题按钮、虚线按钮以及危险按钮。"
+    :code="code"
+  >
+    <Button levle="default" style="margin-right: 20px">Default</Button>
+    <Button levle="primary" style="margin-right: 20px">Primary</Button>
+    <Button levle="dashed" style="margin-right: 20px">Dashed</Button>
+    <Button levle="danger">Danger</Button>
+  </Example>
 </template>
 
 <script lang="ts">
 import { ref } from "vue";
 import Button from "../Button.vue";
+import Example from "../../utils/Example.vue";
 export default {
-  components: { Button: Button },
+  components: { Button: Button, Example },
   setup() {
-    const codeVisible = ref(false);
-    const changeCodeVisible = () => {
-      codeVisible.value = !codeVisible.value;
-    };
-    const codeTipVisible = ref(false);
-    const changeCodeTipVisible = () => {
-      codeTipVisible.value = !codeTipVisible.value;
-    };
-    return {
-      codeVisible,
-      changeCodeVisible,
-      codeTipVisible,
-      changeCodeTipVisible,
-    };
+    const code =
+      `<template>
+  <Button levle="default">Default</Button>
+  <Button levle="primary">Primary</Button>
+  <Button levle="dashed">Dashed</Button>
+  <Button levle="danger">Danger</Button>
+</template>
+    
+<script>
+import Button from "./Button.vue";
+export default {
+  components: { Button }
+}
+<` + "/script>";
+    return { code };
   },
 };
 </script>

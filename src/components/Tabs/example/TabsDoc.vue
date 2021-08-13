5581e1c53eb4b881ab7e07ff83f98aeb9c07bfc9
wuwenxing <1428560819@qq.com>
2021-08-13 22:08:47 +0800

    新增 API表格

--- src/components/Tabs/example/TabsDoc.vue
+++ src/components/Tabs/example/TabsDoc.vue
@@ -1,5 +1,32 @@
 <template>
   <TabsDefault></TabsDefault>
+  <section>
+    <h1>API</h1>
+    <table class="api-table" cellSpacing="0" cellPadding="0">
+      <thead>
+        <tr>
+          <th class="api-table-head">API</th>
+          <th class="api-table-head">说明</th>
+          <th class="api-table-head">类型</th>
+          <th class="api-table-head">默认值</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="api">selected</td>
+          <td class="api-description">当前选中的 Tab</td>
+          <td class="api-type">String</td>
+          <td class="api-default">---</td>
+        </tr>
+        <tr>
+          <td class="api">direction</td>
+          <td class="api-description">Tab 位置</td>
+          <td class="api-type">"top" | "left" | "bottom" | "right"</td>
+          <td class="api-default">top</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script lang="ts">
@@ -10,3 +37,7 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+@import "../../../style/example.scss";
+</style>
